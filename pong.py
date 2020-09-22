cbde4cc527976fdf116366ace4de833c00fc9ccb
korrawee <korrawee.somyos@gmail.com>
2020-09-22 12:59:23 +0700

    Add: moving and bouncing method

--- pong.py
+++ pong.py
@@ -1,24 +1,37 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.clock import Clock
 from kivy.vector import Vector
 
 class PongBall(Widget):
-    velocity_x = NumericProperty(0)
-    velocity_y = NumericProperty(0)
+    velocity_x = NumericProperty(5)
+    velocity_y = NumericProperty(5)
     velocity = ReferenceListProperty(velocity_x, velocity_y)
 
     def move(self):
         self.pos = Vector(*self.velocity) + self.pos
 
 class PongGame(Widget):
-    pass
+    ball = ObjectProperty(None)
 
+    def update(self, dt):
+        self.ball.move()
+
+        # bounce off top and bottom
+        if (self.ball.y < 0) or (self.ball.top > self.height):
+            self.ball.velocity_y *= -1
+
+        # bounce off left and right
+        if (self.ball.x < 0) or (self.ball.right > self.width):
+            self.ball.velocity_x *= -1
 
 class PongApp(App):
-    def build(self):
-        return PongGame()
 
+    def build(self):
+        game = PongGame()
+        Clock.schedule_interval(game.update, 1.0/60.0)
+        return game
 
 if __name__ == '__main__':
     PongApp().run()
